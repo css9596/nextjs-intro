33c9e1cc486da500f321ff546ed5e3bd9efe1dc5
Choi Sung Sik <css9596@naver.com>
2022-02-13 03:27:24 +0900

    Updates

---
+++ server/pages/_app.js
@@ -0,0 +1,223 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 607:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ App)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "styled-jsx/style"
+var style_ = __webpack_require__(816);
+var style_default = /*#__PURE__*/__webpack_require__.n(style_);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(664);
+;// CONCATENATED MODULE: external "next/router"
+const router_namespaceObject = require("next/router");
+;// CONCATENATED MODULE: ./components/NavBar.js
+
+
+
+
+function NavBar() {
+    const router = (0,router_namespaceObject.useRouter)();
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)("nav", {
+        className: "jsx-788837601a257030",
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("img", {
+                src: "/bonobono.png",
+                alt: "보노보노",
+                className: "jsx-788837601a257030"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(next_link["default"], {
+                href: "/",
+                /*#__PURE__*/ children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                    className: "jsx-788837601a257030" + " " + ((router.pathname === "/" ? "active" : "") || ""),
+                    children: "Home"
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(next_link["default"], {
+                href: "/about",
+                children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                    className: "jsx-788837601a257030" + " " + ((router.pathname === "/about" ? "active" : "") || ""),
+                    children: "About"
+                })
+            }),
+            jsx_runtime_.jsx((style_default()), {
+                id: "788837601a257030",
+                children: "nav.jsx-788837601a257030{display:-webkit-box;\ndisplay:-webkit-flex;\ndisplay:-ms-flexbox;\ndisplay:flex;\ngap:10px;\n-webkit-flex-direction:column;\n-ms-flex-direction:column;\nflex-direction:column;\n-webkit-align-items:center;\n-webkit-box-align:center;\n-ms-flex-align:center;\nalign-items:center;\npadding-top:20px;\npadding-bottom:10px;\nbox-shadow:rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px}\nimg.jsx-788837601a257030{max-width:100px;\nmargin-bottom:5px}\nnav.jsx-788837601a257030 a.jsx-788837601a257030{font-weight:600;\nfont-size:18px}\n.active.jsx-788837601a257030{color:tomato}\nnav.jsx-788837601a257030 div.jsx-788837601a257030{display:-webkit-box;\ndisplay:-webkit-flex;\ndisplay:-ms-flexbox;\ndisplay:flex;\ngap:10px}"
+            })
+        ]
+    }));
+};
+
+;// CONCATENATED MODULE: ./components/Layout.js
+
+
+function LayOut({ children  }) {
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(NavBar, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                children: children
+            })
+        ]
+    }));
+};
+
+;// CONCATENATED MODULE: ./pages/_app.js
+
+
+
+function App({ Component , pageProps  }) {
+    return(/*#__PURE__*/ jsx_runtime_.jsx(LayOut, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+            ...pageProps
+        })
+    }));
+};
+
+
+/***/ }),
+
+/***/ 562:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 14:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 20:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 565:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 365:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-middleware-regex.js");
+
+/***/ }),
+
+/***/ 428:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 292:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 52:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 226:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 422:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 816:
+/***/ ((module) => {
+
+module.exports = require("styled-jsx/style");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [730,664], () => (__webpack_exec__(607)));
+module.exports = __webpack_exports__;
+
+})();
