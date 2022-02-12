9cdc07769341045d4adc0b662272e7b0ff3cb7e9
Choi Sung Sik <css9596@naver.com>
2022-02-13 03:56:46 +0900

    redirects, rewrites에 대해서 학습 redirects는 어떠한 URL로 접속을 했을 때 Redirect 처리를 하는 것이고, rewrites는 url을 변경하지 않고 다른 url을 실행시킨다.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source:"/contact",
+        destination:"https://github.com/css9596/",
+        permanent:false,
+      }
+    ]
+  },
+  async rewrites(){
+    return [
+        {
+          source: "/api/movies",
+          destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        },
+    ]
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Seo from "../components/Seo";
 import {useEffect, useState} from "react";
 
-const API_KEY = "468cbaaaf68ece05801898f9fb17121c";
-
 export default function Home(){
     const [movies, setMovies] = useState();
     useEffect(() => {
         (async () => {
             const { results } = await (
                 await fetch(
-                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                `/api/movies`
                 )
             ).json();
             setMovies(results);
@@ -17,38 +15,36 @@ export default function Home(){
         })();
     }, []);
     return (
-        <div>
-            <Seo title="Home"/>
+        <div className="container">
+            <Seo title="Home" />
             {!movies && <h4>Loading...</h4>}
-            {
-                movies?.map((movie) => (
-                    <div key={movie.id}>
-                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                        <h4>{movie.original_title}</h4>
-                    </div>
-                ))
-            }
+            {movies?.map((movie) => (
+                <div className="movie" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
             <style jsx>{`
-             .container {
-               display: grid;
-               grid-template-columns: 1fr 1fr;
-               padding: 20px;
-               gap: 20px;
-             }
-             .movie img {
-               max-width: 100%;
-               border-radius: 12px;
-               transition: transform 0.2s ease-in-out;
-               box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-             }
-             .movie:hover img {
-               transform: scale(1.05) translateY(-10px);
-             }
-             .movie h4 {
-               font-size: 18px;
-               text-align: center;
-             }
-            `}</style>
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
         </div>
     );
 }
